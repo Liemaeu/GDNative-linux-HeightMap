6b0911edef2ed5087716d0005fe57794a71f3e84
Liemaeu <30625460+Liemaeu@users.noreply.github.com>
2020-06-21 11:12:18 +0200

    Update GDNative-linux.sh

--- GDNative-linux.sh
+++ GDNative-linux.sh
@@ -9,5 +9,5 @@ cd godot-cpp
 scons platform=linux generate_bindings=yes
 cd ..
 scons platform=linux
-sed -i '/"Windows": true/a \        \"Linux": true' factory.gd
+sed -i '/"Windows": true/a \        \,"Linux": true' factory.gd
 sed -i '/hterrain_native.dll/a X11.64 = "res://addons/zylann.hterrain/native/bin/linux/libhterrain_native.so" ' hterrain.gdnlib
